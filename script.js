c71d4fa8292a7c20c6c5d0da228463c65a4fb901
H <sarruhassan@gmail.com>
2026-08-06 20:01:43 +0530

    Updated portofolio

--- script.js
+++ script.js
@@ -86,15 +86,6 @@ if (contactForm) {
 }
 
 
-// Mouse glow
-const glow = document.querySelector(".cursor-glow");
-
-if (glow) {
-    document.addEventListener("mousemove", (e) => {
-        glow.style.left = e.clientX + "px";
-        glow.style.top = e.clientY + "px";
-    });
-}
 const backToTop = document.getElementById("backToTop");
 
 window.addEventListener("scroll", () => {
